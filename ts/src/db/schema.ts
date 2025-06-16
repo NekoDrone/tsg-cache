b51eb321aeb6b08ef8696574ebbf5572014a9534
Sylfr Tan <sylfr.serenity.tan@gmail.com>
2025-06-17 01:21:35 +0800

    refactor: more schema changes

--- ts/src/db/schema.ts
+++ ts/src/db/schema.ts
@@ -1,7 +1,9 @@
 import { articlesTable } from "@/db/schema/articles";
+import { navItemsTable } from "./schema/navItems";
 
 export const schema = {
     articlesTable,
+    navItemsTable,
 };
 
 export * from "@/db/schema/articles";
